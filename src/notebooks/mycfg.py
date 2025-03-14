082c9c219be3e9ef587fa88b6ce2502a2a7acaa4
Marc Hanheide <marc@hanheide.net>
2025-03-14 18:08:57 +0000

    Add custom CSS and templates for slide presentations; update introduction notebook

--- src/notebooks/mycfg.py
+++ src/notebooks/mycfg.py
@@ -2,12 +2,18 @@ c = get_config()  #noqa
 
 
 c.NbConvertApp.export_format = 'slides'
-c.TemplateExporter.exclude_input = True
+
+c.TemplateExporter.exclude_input = False
 c.NbConvertApp.notebooks = [
-    "introduction.ipynb",
-    "kalman.ipynb"
+    "*.ipynb"
 ]
 c.SlidesExporter.reveal_theme = 'white'
 c.SlidesExporter.reveal_scroll = True
 c.SlidesExporter.reveal_number = 'c/t'
-#c.SlidesExporter.reveal_height = '200'
+#c.SlidesExporter.reveal_height = '800'
+#c.SlidesExporter.reveal_height = '450'
+
+c.SlidesExporter.extra_template_paths = ['/Users/mhanheide/workspace/trajectory_estimation_lecture/src/notebooks/templates/']
+c.SlidesExporter.extra_template_basedirs=["/Users/mhanheide/workspace/trajectory_estimation_lecture/src/notebooks/templates/"]
+
+c.TemplateExporter.template_name = 'marc_reveal'
---
+++ src/notebooks/templates/slides_reveal.tpl
@@ -0,0 +1,6 @@
+{% extends "reveal/index.html.j2" %}
+
+{% block header %}
+{{ super() }}
+<link rel="stylesheet" href="custom.css">
+{% endblock %}
