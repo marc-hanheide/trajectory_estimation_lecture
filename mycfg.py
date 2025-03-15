b1d902d5bf287bbfd48bf86e2c612a3d9d856fe9
Marc Hanheide <marc@hanheide.net>
2025-03-15 14:28:13 +0000

    Add new assets and templates for slide presentations; include custom CSS and example notebook

--- mycfg.py
+++ mycfg.py
@@ -13,7 +13,7 @@ c.SlidesExporter.reveal_number = 'c/t'
 #c.SlidesExporter.reveal_height = '800'
 #c.SlidesExporter.reveal_height = '450'
 
-c.SlidesExporter.extra_template_paths = ['/Users/mhanheide/workspace/trajectory_estimation_lecture/src/notebooks/templates/']
-c.SlidesExporter.extra_template_basedirs=["/Users/mhanheide/workspace/trajectory_estimation_lecture/src/notebooks/templates/"]
+c.SlidesExporter.extra_template_paths = ['./templates/']
+c.SlidesExporter.extra_template_basedirs=["./templates/"]
 
 c.TemplateExporter.template_name = 'marc_reveal'
